29cf42931619351e5b51fc652c400b2e5df90eea
eunsong <0rwt0yqi@naver.com>
2020-10-21 16:41:23 +0900

    cam

---
+++ testWebrtc/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import LoginScreen from './screens/LoginScreen';
+import CallScreen from './screens/CallScreen';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Call" 
+          component={CallScreen} 
+          options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
