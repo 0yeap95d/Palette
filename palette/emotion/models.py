feeceea398c15399a23800fa5eee0937e3812e87
khj <skawk7872@naver.com>
2020-10-27 13:47:29 +0900

    up

---
+++ palette/emotion/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+class Emotion(models.Model):
+    userNo = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='emotion')
+    mood1 = models.FloatField()
+    mood2 = models.FloatField()
+    mood3 = models.FloatField()
+    mood4 = models.FloatField()
+    mood5 = models.FloatField()
+    mood6 = models.FloatField()
+    mood7 = models.FloatField()
+    mood8 = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+class Result(models.Model):
+    moodType = models.IntegerField(blank=True, null=True)
+    resultType = models.IntegerField(blank=True, null=True)
+    content = models.CharField(max_length=100)
