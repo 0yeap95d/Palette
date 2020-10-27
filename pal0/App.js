527ee33c4f87a4c323e2eb1698f8852e548f4b74
eunsong <0rwt0yqi@naver.com>
2020-10-27 15:33:45 +0900

    front | 메인 & 사용자 정보 입력 | S03P31D102-26

---
+++ pal0/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import MainScreen from './page/MainScreen';  // 메인 로그인 버튼 
+import JoinScreen from './page/JoinScreen';  // 닉네임 성별 나이 기입 
+// import InsertInfoScreen from './page/InsertInfoScreen'; // 닉네임, 성별, 나이 기입
+// import CallScreen from './page/CallScreen'; 
+// import CameraScreen from './page/CameraScreen'; // 영상 & 음성
+// import ResultScreen from './page/ResultScreen'; // 결과 페이지
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main" 
+          component={MainScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Join" 
+          component={JoinScreen}
+          options={{headerShown: false}}
+        />
+        {/* <Stack.Screen 
+          name="Info" 
+          component={InsertInfoScreen} 
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Call" 
+          component={CallScreen} 
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Camera" 
+          component={CameraScreen} 
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Result" 
+          component={ResultScreen} 
+          options={{headerShown: false}}
+        />   */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
