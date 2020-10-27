e44f5d294a72552b8b07cb8d80b3f97872daf366
eunsong <0rwt0yqi@naver.com>
2020-10-27 16:39:25 +0900

    temp

--- testWebrtc/App.js
+++ testWebrtc/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import LoginScreen from './screens/LoginScreen';
-import CallScreen from './screens/CallScreen';
+import MainScreen from './screens/MainScreen';  // 메인 로그인 버튼 
+import LoginScreen from './screens/LoginScreen';  // 메인 로그인 버튼 
+import InsertInfoScreen from './screens/InsertInfoScreen'; // 닉네임, 성별, 나이 기입
+import CallScreen from './screens/CallScreen'; 
+import CameraScreen from './screens/CameraScreen'; // 영상 & 음성
+import ResultScreen from './screens/ResultScreen'; // 결과 페이지
 
 
 const Stack = createStackNavigator();
@@ -13,14 +17,35 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Login"
+          name="Main" 
+          component={MainScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Login" 
           component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Info" 
+          component={InsertInfoScreen} 
           options={{headerShown: false}}
         />
         <Stack.Screen 
           name="Call" 
           component={CallScreen} 
-          options={{headerShown: false}}/>
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Camera" 
+          component={CameraScreen} 
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Result" 
+          component={ResultScreen} 
+          options={{headerShown: false}}
+        />  
       </Stack.Navigator>
     </NavigationContainer>
   );
--- testWebrtc/screens/InsertInfoScreen.js
+++ testWebrtc/screens/InsertInfoScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ImageBackground} from 'react-native';
 import {Text} from 'react-native-paper';
 import {TextInput} from 'react-native-paper';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -9,7 +9,7 @@ export default function LoginScreen(props) {
   const [userId, setUserId] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const onLogin = async () => {
+  const checkLogin = async () => {
     setLoading(true);
     try {
       await AsyncStorage.setItem('userId', userId);
@@ -23,25 +23,19 @@ export default function LoginScreen(props) {
 
   return (
     <View style={styles.root}>
-      <View style={styles.content}>
-        <Text style={styles.heading}>Enter your id</Text>
-        <TextInput
-          label="Your  ID"
-          onChangeText={text => setUserId(text)}
-          mode="outlined"
-          style={styles.input}
-        />
-
+      <ImageBackground 
+      style={styles.content}
+      source={require("./assets/img/main.png")}
+      resizeMode="cover"
+      >
+        <Text style={styles.logotxt}></Text>
         <Button
           mode="contained"
-          onPress={onLogin}
-          loading={loading}
-          style={styles.btn}
-          contentStyle={styles.btnContent}
-          disabled={userId.length === 0}>
-          Login
+          onPress={checkLogin}
+          loading={loading}>
+          시작하기
         </Button>
-      </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -50,32 +44,16 @@ const styles = StyleSheet.create({
   root: {
     backgroundColor: '#fff',
     flex: 1,
-    // alignItems: 'center',
     justifyContent: 'center',
   },
   content: {
-    // alignSelf: 'center',
-    paddingHorizontal: 20,
-    justifyContent: 'center',
+    width: 100,
+    height : 100
   },
-  heading: {
-    fontSize: 18,
-    marginBottom: 10,
-    fontWeight: '600',
-  },
-  input: {
-    height: 60,
-    marginBottom: 10,
-  },
-  btn: {
+  logotxt: {
     height: 60,
     alignItems: 'stretch',
     justifyContent: 'center',
-    fontSize: 18,
-  },
-  btnContent: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 60,
+    fontSize: 30,
   },
 });
