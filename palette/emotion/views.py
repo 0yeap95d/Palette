a7e453247e1db12621680c5600a1f2b4425ca17f
khj <skawk7872@naver.com>
2020-10-30 15:00:18 +0900

    uuu

--- palette/emotion/views.py
+++ palette/emotion/views.py
@@ -329,8 +329,11 @@ def question(request):
         'que' : random_que.question
     })
 
-global X_dialog
-def ex(request):
+    
+@api_view(['POST'])
+@csrf_exempt
+def text(request):
+
     dialog_data = pd.read_csv('./models/dialogSentence.csv')
     dialog_data['Sentence'].nunique(), dialog_data['Emotion'].nunique()
     dialog_data = dialog_data.dropna(subset=['Emotion'])
@@ -339,6 +342,7 @@ def ex(request):
     dialog_data = dialog_data.dropna(subset=['Sentence'])
     stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
     okt = Okt()
+
     X_dialog = []
     
     for sentence in dialog_data['Sentence']:
@@ -346,12 +350,6 @@ def ex(request):
         temp_X = okt.morphs(sentence, stem=True) # 토큰화
         temp_X = [word for word in temp_X if not word in stopwords] # 불용어 제거
         X_dialog.append(temp_X)
-
-@api_view(['POST'])
-@csrf_exempt
-def text(request):
-
-    
     print(X_dialog)
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(X_dialog)
@@ -436,7 +434,7 @@ def text(request):
         else:
             print("{:.2f}% 확률로 행복 입니다.\n".format(score*100))
             
-    sentiment_predict('굉장히 무섭다')
+    sentiment_predict(text)
 
     print('------------------------------------------------------------------끝')
 
